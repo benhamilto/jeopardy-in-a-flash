6ba0ee1943f49571b37ce7b72d30ebb40499eabe
Ben Hamilton <benhamilton10@gmail.com>
2019-11-10 21:36:46 -0500

    Change to get 5 clues for category, update behavior
    - Retrieves 5 clues at a time and goes through all 5
    before moving on to next category
    - Add behavior for keypresses to move to next game state
    - Make styling slightly more mobile friendly

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-  getRandomClue();
+let historyMap = new Map();
+let disableNextState = false;
+let currentClues = [];
+
+const States = Object.freeze({
+  Question: "question",
+  Answer: "answer"
 });
+let currentState = States.Answer;
+
+document.addEventListener("DOMContentLoaded", function() {
+  nextState();
+});
+
+document.onkeyup = e => {
+  if (disableNextState !== true) {
+    e = e || window.event;
+
+    // right arrow or 'l'
+    if (e.keyCode === 39 || e.keyCode === 76) {
+      nextState();
+    }
+  }
+};
 
 let showQuestion = () => {
   document.querySelector("#question").classList.remove("hidden");
@@ -12,29 +33,90 @@ let showAnswer = () => {
   document.querySelector("#answer").classList.remove("hidden");
 };
 
-let setQuestion = questionText => {
-  document.querySelector("#question > .box").innerHTML = questionText;
+let nextState = () => {
+  if (currentState === States.Question) {
+    showAnswer();
+    currentState = States.Answer;
+  } else {
+    nextQuestion();
+    currentState = States.Question;
+  }
 };
 
-let setAnswer = answerText => {
-  document.querySelector("#answer > .box").innerHTML = answerText;
+/*
+ * Changes escaped apostrophes from Ben\'s to Ben's
+ */
+let removeEscapeFromQuote = text => text.replace(/\\'/g, "'");
+
+let setQuestionText = questionText => {
+  document.querySelector("#question > .box").innerHTML = removeEscapeFromQuote(
+    questionText
+  );
 };
 
-let setCategory = categoryTitle => {
-  document.querySelector("#category").innerHTML = categoryTitle;
+let setAnswerText = answerText => {
+  document.querySelector("#answer > .box").innerHTML = removeEscapeFromQuote(
+    answerText
+  );
+};
+
+let setCategoryText = categoryTitle => {
+  document.querySelector("#category").innerHTML = removeEscapeFromQuote(
+    categoryTitle
+  );
 };
 
 let getRandomClue = async () => {
-  const response = await fetch("https://cors-anywhere.herokuapp.com/http://jservice.io/api/random?count=1", {
-    headers: {
-      'Origin': 'https://benhamilto.github.io/jeopardy-in-a-flash/'
+  const response = await fetch(
+    "https://cors-anywhere.herokuapp.com/http://jservice.io/api/random?count=1",
+    {
+      headers: {
+        Origin: "https://benhamilto.github.io/jeopardy-in-a-flash/"
+      }
     }
-  });
+  );
   const clueArr = await response.json();
   const clue = clueArr[0];
 
-  setQuestion(clue.question);
-  setAnswer(clue.answer);
-  setCategory(clue.category.title);
+  return clue;
+};
+
+let getRandomCategoryClues = async () => {
+  const randomClue = await getRandomClue();
+  const categoryId = randomClue.category.id;
+  const response = await fetch(
+    `https://cors-anywhere.herokuapp.com/http://jservice.io/api/clues?category=${categoryId}`,
+    {
+      headers: {
+        Origin: "https://benhamilto.github.io/jeopardy-in-a-flash/"
+      }
+    }
+  );
+
+  let categoryClues = await response.json();
+  categoryClues = categoryClues.slice(0, 5);
+  return categoryClues;
+};
+
+let nextQuestion = async () => {
+  if (disableNextState === true) {
+    return;
+  }
+
+  disableNextState = true;
+
+  let clue = currentClues.shift();
+  if (!clue) {
+    currentClues = await getRandomCategoryClues();
+    clue = currentClues.shift();
+  }
+
+  disableNextState = false;
+
+  setQuestionText(clue.question);
+  setAnswerText(clue.answer);
+  setCategoryText(clue.category.title);
   showQuestion();
 };
+
+let nextAnswer = async () => {};
